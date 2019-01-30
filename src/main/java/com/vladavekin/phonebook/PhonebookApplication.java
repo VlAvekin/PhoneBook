a9d7bf632d81cb1dde98874aa845f3c9d1a8bcca
VlAvekin <vlad2002cat95git>
2019-01-30 21:21:14 +0200

    Initial project

---
+++ src/main/java/com/vladavekin/phonebook/PhonebookApplication.java
@@ -0,0 +1,15 @@
+package com.vladavekin.phonebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PhonebookApplication.class, args);
+    }
+
+}
+
