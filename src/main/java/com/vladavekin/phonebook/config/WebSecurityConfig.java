193cf97d209d1f60b9614d66d852c7e2d8a6d306
VlAvekin <vlad2002cat95git>
2019-02-05 01:11:31 +0200

    Spring Session

--- src/main/java/com/vladavekin/phonebook/config/WebSecurityConfig.java
+++ src/main/java/com/vladavekin/phonebook/config/WebSecurityConfig.java
@@ -23,15 +23,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/registration", "/static/**").permitAll()
-                .anyRequest().authenticated()
+                    .antMatchers("/", "/registration", "/static/**").permitAll()
+                    .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
+                    .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
                 .and()
-                .logout()
-                .permitAll();
+                    .rememberMe()
+                .and()
+                    .logout()
+                    .permitAll();
     }
 
     @Override
